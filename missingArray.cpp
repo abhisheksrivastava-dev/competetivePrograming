7f36ba3fa5e9f33e8a9e0ef74dc0f771794521ae
Abhishek Srivastava <74584768+abhisheksrivastava-dev@users.noreply.github.com>
2021-11-05 10:14:28 +0530

    Update missingArray.cpp

--- missingArray.cpp
+++ missingArray.cpp
@@ -6,7 +6,18 @@
   Explanation: 4 and 10 are present in 
   first array, but not in second array.
 */
-map<long long int,long long int> m1;
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+	
+	
+	public:
+	vector<long long> findMissing(long long a[], long long b[],  
+                 int n, int m) 
+	{ 
+	    map<long long int,long long int> m1;
 	    for(int i=0;i<m;i++)
 	    {
 	        m1[b[i]]++;
@@ -18,3 +29,41 @@ map<long long int,long long int> m1;
 	        v.push_back(a[i]);
 	    } 
 	    return v;
+	} 
+};
+	
+
+// { Driver Code Starts.
+
+int main() 
+{
+   	
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+    	int n;
+        cin>>n;
+        int m;
+        cin>>m;
+        long long a[n];
+        long long b[m];
+        for(int i=0;i<n;i++)
+        cin>>a[i];
+        for(int i=0;i<m;i++)
+        cin >> b[i];
+        vector<long long> ans;
+        
+
+        Solution ob;
+        ans=ob.findMissing(a,b,n,m);
+        for(int i=0;i<ans.size();i++)
+        cout<<ans[i]<<" ";
+	    
+        
+	    cout << "\n";
+	     
+    }
+    return 0;
+}  // } Driver Code Ends
