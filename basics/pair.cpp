f8659bd01204e3091bda85cc0f3140d45410107a
Abhishek Srivastava <74584768+abhisheksrivastava-dev@users.noreply.github.com>
2022-01-18 17:59:53 +0530

    Create pair.cpp

---
+++ basics/pair.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	pair<int, string> p;
+	// p = make_pair(1,"Hello");
+	p = {1, "abc"};
+	cout << p.first << " " << p.second << endl;
+	int a[] = {1,2,3};
+	int b[] = {2,3,4};
+	pair<int, int> p_array[3];
+	p_array[0] = {1,2};
+	p_array[1] = {2,3};
+	p_array[2] = {3,4};
+	swap(p_array[0], p_array[2]);
+	for(int i = 0;i < 3;++i){
+		cout << p_array[i].first << " " << p_array[i].second << endl;
+	}
+}
